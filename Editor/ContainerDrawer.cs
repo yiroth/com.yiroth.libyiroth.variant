a3ff0ec73f3d7053c269d6232369d962156a2f46
nokklair <nokklair@proton.me>
2025-12-12 13:32:03 +0300

    auto

---
+++ Editor/ContainerDrawer.cs
@@ -0,0 +1,90 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace LibYiroth.Variant.Editor
+{
+    [CustomPropertyDrawer(typeof(Container))]
+    public class ContainerDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+
+            // Calculate Rects with proper spacing
+            float spacing = 5f;
+            float fullWidth = position.width;
+            
+            // Layout: [Name (30%)]  [Type (80px)]  [Value (Rest)]
+            float nameWidth = fullWidth * 0.3f;
+            float typeWidth = 80f;
+            float valueWidth = fullWidth - nameWidth - typeWidth - (spacing * 2);
+
+            Rect nameRect = new Rect(position.x, position.y, nameWidth, position.height);
+            Rect typeRect = new Rect(position.x + nameWidth + spacing, position.y, typeWidth, position.height);
+            Rect valueRect = new Rect(position.x + nameWidth + typeWidth + (spacing * 2), position.y, valueWidth, position.height);
+
+            // 1. Draw Name Field
+            SerializedProperty nameProp = property.FindPropertyRelative("name");
+            if (nameProp != null)
+            {
+                // GUIContent.none ensures we just get the text box, which prevents layout locking
+                EditorGUI.PropertyField(nameRect, nameProp, GUIContent.none);
+            }
+            else
+            {
+                EditorGUI.LabelField(nameRect, "Name Missing");
+            }
+
+            // 2. Draw Type Dropdown & Value
+            SerializedProperty variantProp = property.FindPropertyRelative("variable");
+            if (variantProp != null)
+            {
+                // Look for _type. If not found, Unity serialization hasn't refreshed or [SerializeField] is missing.
+                SerializedProperty typeProp = variantProp.FindPropertyRelative("_type");
+
+                if (typeProp != null)
+                {
+                    // Draw Type Dropdown
+                    EditorGUI.PropertyField(typeRect, typeProp, GUIContent.none);
+
+                    // Draw the correct Value field
+                    VariantTypes currentType = (VariantTypes)typeProp.enumValueIndex;
+                    SerializedProperty valueProp = null;
+
+                    switch (currentType)
+                    {
+                        case VariantTypes.Int:
+                            valueProp = variantProp.FindPropertyRelative("intValue");
+                            break;
+                        case VariantTypes.Float:
+                            valueProp = variantProp.FindPropertyRelative("floatValue");
+                            break;
+                        case VariantTypes.Bool:
+                            valueProp = variantProp.FindPropertyRelative("boolValue");
+                            break;
+                        case VariantTypes.String:
+                            valueProp = variantProp.FindPropertyRelative("stringValue");
+                            break;
+                    }
+
+                    if (valueProp != null)
+                    {
+                        EditorGUI.PropertyField(valueRect, valueProp, GUIContent.none);
+                    }
+                }
+                else
+                {
+                    EditorGUI.LabelField(typeRect, "Serialize Err", EditorStyles.miniLabel);
+                    EditorGUI.LabelField(valueRect, "Missing [SerializeField] in Variant.cs?", EditorStyles.miniLabel);
+                }
+            }
+
+            EditorGUI.EndProperty();
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUIUtility.singleLineHeight;
+        }
+    }
+}
---
+++ Scripts/Variant.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+
+namespace LibYiroth.Variant
+{
+    public enum VariantTypes
+    {
+        Empty,
+        Int,
+        Float,
+        Bool,
+        String
+    }
+
+    [System.Serializable]
+    public struct Variant
+    {
+        [SerializeField] private VariantTypes type;
+
+        [SerializeField] private float floatValue;
+        [SerializeField] private int intValue;
+        [SerializeField] private bool boolValue;
+        [SerializeField] private string stringValue;
+
+        public Variant(object value) : this()
+        {
+            if (value == null) { type = VariantTypes.Empty; return; }
+            
+            switch (value)
+            {
+                case float f: Set<float>(f); break;
+                case int i: Set<int>(i); break;
+                case bool b: Set<bool>(b); break;
+                case string s: Set<string>(s); break;
+                default: type = VariantTypes.Empty; break;
+            }
+        }
+
+        public Variant(float value) : this() => Set<float>(value);
+        public Variant(int value) : this() => Set<int>(value);
+        public Variant(bool value) : this() => Set<bool>(value);
+        public Variant(string value) : this() => Set<string>(value);
+
+        public void Set<T>(T v)
+        {
+            if (v.GetType() is T direct)
+            {
+                switch (direct)
+                {
+                    case float f:
+                        type = VariantTypes.Float;
+                        floatValue = f;
+                        return;
+                    case int i:
+                        type = VariantTypes.Int;
+                        intValue = i;
+                        return;
+                    case bool b:
+                        type = VariantTypes.Bool;
+                        boolValue = b;
+                        return;
+                    case string s:
+                        type = VariantTypes.String;
+                        stringValue = s;
+                        return;
+                }
+            }
+
+            type = VariantTypes.Empty;
+        }
+
+        public new VariantTypes GetType() => type;
+
+        public object GetRawValue() => type switch
+        {
+            VariantTypes.Int => intValue,
+            VariantTypes.Float => floatValue,
+            VariantTypes.Bool => boolValue,
+            VariantTypes.String => stringValue,
+            _ => null
+        };
+        
+        public T GetValue<T>()
+        {
+            if (typeof(T) == typeof(float)) return (T)(object)floatValue;
+            if (typeof(T) == typeof(int)) return (T)(object)intValue;
+            if (typeof(T) == typeof(bool)) return (T)(object)boolValue;
+            if (typeof(T) == typeof(string)) return (T)(object)stringValue;
+
+            try { return (T)System.Convert.ChangeType(GetRawValue(), typeof(T)); }
+            catch { return default; }
+        }
+
+        public static implicit operator float(Variant v) => v.GetValue<float>();
+        public static implicit operator Variant(float f) => new Variant(f);
+        public static implicit operator int(Variant v) => v.GetValue<int>();
+        public static implicit operator Variant(int i) => new Variant(i);
+        public static implicit operator bool(Variant v) => v.GetValue<bool>();
+        public static implicit operator Variant(bool b) => new Variant(b);
+        public static implicit operator string(Variant v) => v.GetValue<string>();
+        public static implicit operator Variant(string s) => new Variant(s);
+    }
+}
