cd1dd17eec95da5085544970c2461f22aaaaf6e4
nokklair <nokklair@proton.me>
2025-12-13 11:03:25 +0300

    Add license and notice files for all packages

--- Editor/ContainerDrawer.cs
+++ Editor/ContainerDrawer.cs
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2025 yiroth
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Purpose: For to keep editor inspector UI clean
+ */
+
 using UnityEditor;
 using UnityEngine;
 
@@ -23,7 +41,7 @@ namespace LibYiroth.Variant.Editor
             Rect typeRect = new Rect(position.x + nameWidth + spacing, position.y, typeWidth, position.height);
             Rect valueRect = new Rect(position.x + nameWidth + typeWidth + (spacing * 2), position.y, valueWidth, position.height);
 
-            // 1. Draw Name Field
+            // Draw Name Field
             SerializedProperty nameProp = property.FindPropertyRelative("name");
             if (nameProp != null)
             {
@@ -35,7 +53,7 @@ namespace LibYiroth.Variant.Editor
                 EditorGUI.LabelField(nameRect, "Name Missing");
             }
 
-            // 2. Draw Type Dropdown & Value
+            // Draw Type Dropdown & Value
             SerializedProperty variantProp = property.FindPropertyRelative("variable");
             if (variantProp != null)
             {
@@ -75,7 +93,7 @@ namespace LibYiroth.Variant.Editor
                 else
                 {
                     EditorGUI.LabelField(typeRect, "Serialize Err", EditorStyles.miniLabel);
-                    EditorGUI.LabelField(valueRect, "Missing [SerializeField] in Variant.cs?", EditorStyles.miniLabel);
+                    EditorGUI.LabelField(valueRect, "Missing [SerializeField] in Variant.cs", EditorStyles.miniLabel);
                 }
             }
 
